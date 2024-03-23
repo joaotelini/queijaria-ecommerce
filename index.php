163dce985e91821c9f03e2aad1990aa75fed90b9
João Pedro Telini <joaotelini03@gmail.com>
2024-03-22 23:05:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
     header('Location: /assets/pages/login.html');
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    aaaaaa
+</body>
+</html>
