1c807041dfb7e4b052e232d2459313354d91251f
João Pedro Telini <joaotelini03@gmail.com>
2024-04-01 22:34:47 -0300

    insert datas on tables

--- php/verify-cad.php
+++ php/verify-cad.php
@@ -1,5 +1,6 @@
 <?php
-    require_once ("connection-sql.php");
+    include_once ("connection-sql.php");
+
     $nm = $_POST["nome"];
     $email = $_POST["email"];
     $senha = $_POST["senha"];
@@ -11,8 +12,8 @@
     if ($nm == "" || $email == "" || $senha == "" || $logradouro == "" || $numero == "" || $bairro == "" || $celular == "") {
         echo "Voce deixou algum campo vazio, volte para preencher corretamente.";
     } else {
-        $sql = "INSERT INTO usuario (nome_completo, email, senha, logradouro, numero, bairro, celular) VALUES ( $nm, $email, $senha, $logradouro, $numero, $bairro, $celular)";
-
+        $sql = "INSERT INTO usuario (nome_completo, email, senha, logradouro, numero, bairro, celular) 
+        VALUES ('$nm', '$email', '$senha', '$logradouro', '$numero', '$bairro', '$celular')";
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
         } else {
