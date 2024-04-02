ec8029ab48f3f63a63f6dd9d707742d4189009e0
João Pedro Telini <joaotelini03@gmail.com>
2024-04-01 21:10:41 -0300

    teste sql query

--- php/connection-sql.php
+++ php/connection-sql.php
@@ -1,10 +1,29 @@
 <?php
-        $host = "localhost";
-        $db = "db_sitio3irmaos";
-        $user = "root";
-        $pass = "";
+    $host = "localhost";
+    $db = "db_sitio3irmaos";
+    $user = "root";
+    $pass = "";
 
-        $mysqli = new mysqli($host, $user, $pass, $db);
-        if ($mysqli->connect_errno) {
-            echo "Falhou: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    $conn = new mysqli($host, $user, $pass, $db);
+    if ($conn->connect_errno) {
+        die($conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM usuario";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "Nome: " . $row["nome_completo"] . 
+            " - E-mail: " . $row["email"] . 
+            " - Logradouro: " . $row["logradouro"] .
+            " @ " . $row ["numero"] .
+            " - Bairro : " . $row["bairro"] .
+            " - Celular: " . $row ["celular"];
         }
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
