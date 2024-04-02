2f30c1177d7162311467c8e44e5a1401651feb2e
João Pedro Telini <joaotelini03@gmail.com>
2024-04-01 22:09:12 -0300

    a

--- php/connection-sql.php
+++ php/connection-sql.php
@@ -12,18 +12,18 @@
     $sql = "SELECT * FROM usuario";
     $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while ($row = $result->fetch_assoc()) {
-            echo "Nome: " . $row["nome_completo"] . 
-            " - E-mail: " . $row["email"] . 
-            " - Logradouro: " . $row["logradouro"] .
-            " @ " . $row ["numero"] .
-            " - Bairro : " . $row["bairro"] .
-            " - Celular: " . $row ["celular"];
-        }
-    } else {
-        echo "0 results";
-    }
+    // if ($result->num_rows > 0) {
+    //     // output data of each row
+    //     while ($row = $result->fetch_assoc()) {
+    //         echo "Nome: " . $row["nome_completo"] . 
+    //         " - E-mail: " . $row["email"] . 
+    //         " - Logradouro: " . $row["logradouro"] .
+    //         " @ " . $row ["numero"] .
+    //         " - Bairro : " . $row["bairro"] .
+    //         " - Celular: " . $row ["celular"];
+    //     }
+    // } else {
+    //     echo "0 results";
+    // }
 
-    $conn->close();
+    // $conn->close();
---
+++ php/verify-cad.php
@@ -0,0 +1,23 @@
+<?php
+    require_once ("connection-sql.php");
+    $nm = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+    $logradouro = $_POST["logradouro"];
+    $numero = $_POST["numero"];
+    $bairro = $_POST["bairro"];
+    $celular = $_POST["celular"];
+
+    if ($nm == "" || $email == "" || $senha == "" || $logradouro == "" || $numero == "" || $bairro == "" || $celular == "") {
+        echo "Voce deixou algum campo vazio, volte para preencher corretamente.";
+    } else {
+        $sql = "INSERT INTO usuario (nome_completo, email, senha, logradouro, numero, bairro, celular) VALUES ( $nm, $email, $senha, $logradouro, $numero, $bairro, $celular)";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+}
