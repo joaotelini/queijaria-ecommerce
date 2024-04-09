051a7906badd1d1b42588f43254679bcd2896d83
João Pedro Telini <joaotelini03@gmail.com>
2024-04-08 23:35:11 -0300

    connect to online db

--- php/connection-sql.php
+++ php/connection-sql.php
@@ -1,29 +1,10 @@
 <?php
     $host = "localhost";
-    $db = "db_sitio3irmaos";
-    $user = "root";
-    $pass = "";
+    $db = "id22020141_db_sitio3irmaos";
+    $user = "id22020141_admin";
+    $pass = "Joaopedro@12";
 
     $conn = new mysqli($host, $user, $pass, $db);
     if ($conn->connect_errno) {
         die($conn->connect_error);
     }
-
-    $sql = "SELECT * FROM usuario";
-    $result = $conn->query($sql);
-
-    // if ($result->num_rows > 0) {
-    //     // output data of each row
-    //     while ($row = $result->fetch_assoc()) {
-    //         echo "Nome: " . $row["nome_completo"] . 
-    //         " - E-mail: " . $row["email"] . 
-    //         " - Logradouro: " . $row["logradouro"] .
-    //         " @ " . $row ["numero"] .
-    //         " - Bairro : " . $row["bairro"] .
-    //         " - Celular: " . $row ["celular"];
-    //     }
-    // } else {
-    //     echo "0 results";
-    // }
-
-    // $conn->close();
