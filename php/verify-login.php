ae7fa7d127a50ccf49baa3563a1fb71d9df857b7
João Pedro Telini <joaotelini03@gmail.com>
2024-04-14 23:03:38 -0300

    reconhecendo usuario cadastrado

--- php/verify-login.php
+++ php/verify-login.php
@@ -4,18 +4,12 @@
     $email = $_POST["email"];
     $senha = $_POST["senha"];
 
-    // $sqlSenha = "SELECT * FROM usuario WHERE senha = '{$senha}'";
-    $sql = "SELECT * from usuario where email = '{$email}' and senha = '{$senha}'";
-    // $sqlEmail = "SELECT * FROM usuario WHERE email = '{$email}'";
+    $sql = "SELECT * from usuario where email = '$email' and senha = '$senha'";
+    $consulta = $conn->query($sql) or die("". $conn->error);
+    $verificacaoDElogin = $consulta->num_rows;
 
-    if ($email = "" || $senha == "") {
-        echo "Voce deixou algum campo vazio";
-    }
-    else {
-        if ($email and $senha = $conn->query($sql)) {
-            echo "usuario existe";
-        }
-        else {
-            echo "nao existe";
-        }
+    if ($verificacaoDElogin == 1) {
+        echo"usuario existe";
+    }else{
+        echo "usuario nao encontrado";
     }
