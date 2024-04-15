d6a4fa922e44335291e9ca8757e8aa195bf7ecaf
João Pedro Telini <joaotelini03@gmail.com>
2024-04-15 13:18:27 -0300

    header to welcome

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     // Verificar se o usuário está logado
     if (!isset($_SESSION['email'])) {
         // Se o usuário não estiver logado, redirecionar para a página de login
-        header('Location: pages/login.html');
+        header('Location: welcome.html');
         exit();
     } else {
         // Se o usuário estiver logado, exibir os dados do usuário
