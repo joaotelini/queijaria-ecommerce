c4c9bcb559096484103db1e6ef71eaaa38692c07
João Pedro Telini <joaotelini03@gmail.com>
2024-04-17 00:09:35 -0300

    connect with mysqli

--- php/connection-sql.php
+++ php/connection-sql.php
@@ -1,11 +1,32 @@
 <?php
-    $host = "localhost";
-    $db = "db_sitio3irmaos";
-    $user = "root";
-    $pass = "";
+$host = "localhost";
+$db = "db_sitio3irmaos";
+$user = "root";
+$pass = "";
 
-    $conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db);
 
-    if ($conn->error) {
-        die($conn->connect_error);
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
+}
+
+// Função para executar consultas SQL preparadas
+function executePreparedQuery($conn, $sql, $types = null, ...$params) {
+    $stmt = $conn->prepare($sql);
+    
+    // Verificar se a preparação da consulta falhou
+    if (!$stmt) {
+        die("Erro na preparação da consulta: " . $conn->error);
     }
+    
+    // Vincular parâmetros à declaração preparada
+    if (!empty($params)) {
+        $stmt->bind_param($types, ...$params);
+    }
+    
+    // Executar a consulta
+    $stmt->execute();
+    
+    // Retornar o resultado da consulta
+    return $stmt->get_result();
+}
